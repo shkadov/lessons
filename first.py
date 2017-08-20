6d076f5217b98ce1fb41d721d83f5e9613a54c72
Den Shkadov <den.shkadov@namecheap.com>
2017-08-20 11:36:21 +0300

    factorial

---
+++ first.py
@@ -0,0 +1,9 @@
+def factorial(x):
+    result = 1
+    for i in range(1, x+1):
+        result = result * i
+    print('Factorial of number '+ str(x) + ' is equal ' + str(result))
+
+x = raw_input("Number: ")
+x = int(x)
+factorial(x)
